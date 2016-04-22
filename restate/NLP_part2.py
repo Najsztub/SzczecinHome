e2367f523b4eff4233cc9c9bc2d1a0deb36c41b6
Mateusz Najsztub <mateusz.najsztub@gmail.com>
2016-04-22 21:58:01 +0200

    Added word2vect model
    Taken with parameters from the tutorial.

--- restate/NLP_part2.py
+++ restate/NLP_part2.py
@@ -37,7 +37,8 @@ print result.summary()
 
 # create prediction errors
 df['pred_error'] = result.resid_pearson
-df['price_class'] = 0 + (result.resid_pearson>.4) - (result.resid_pearson<-.4)
+df['price_class'] = 0 + (result.resid_pearson>.4) \
+                    - (result.resid_pearson<-.4)
 
 print "Cases identified as 0: neutral, -1: over and 1: underpriced"
 print df.price_class.value_counts()
@@ -92,9 +93,39 @@ sentences = []
 print "Parsing sentences from training data"
 for desc in train['description']:
     sentences += desc_to_sentences(desc, tokenizer)
-print "Parsing sentences from t data"
+print "Parsing sentences from test data"
 for desc in test['description']:
     sentences += desc_to_sentences(desc, tokenizer)
 
 print "We have %d sentences in test and training data"%(len(sentences))
+'''
+Word2Vect model part
+'''
 
+# Import logging features
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+    level=logging.INFO)
+
+# Set values for various parameters
+num_features = 300    # Word vector dimensionality                      
+min_word_count = 40   # Minimum word count                        
+num_workers = 4       # Number of threads to run in parallel
+context = 10          # Context window size
+downsampling = 1e-3   # Downsample setting for frequent words
+
+# Initialize and train the model (this will take some time)
+from gensim.models import word2vec
+print "Training model..."
+model = word2vec.Word2Vec(sentences, workers=num_workers, \
+            size=num_features, min_count = min_word_count, \
+            window = context, sample = downsampling)
+
+# If you don't plan to train the model any further, calling 
+# init_sims will make the model much more memory-efficient.
+model.init_sims(replace=True)
+
+# It can be helpful to create a meaningful model name and 
+# save the model for later use. You can load it later using Word2Vec.load()
+model_name = "300features_40minwords_10context"
+model.save(model_name)
