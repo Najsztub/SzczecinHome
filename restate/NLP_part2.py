374447ff96a4ecfb9e16d7ca2ae09bff404a712d
Mateusz Najsztub <mateusz.najsztub@gmail.com>
2016-04-22 22:26:02 +0200

    Finished NLP2
    Added final model loading, saving and some examples for the trained
    model.

--- restate/NLP_part2.py
+++ restate/NLP_part2.py
@@ -11,7 +11,15 @@ Import data from 080416 as data to fit
 import pandas as pd
 import codecs
 import re
+import sys
 from data.data import clean_data
+
+
+if len(sys.argv) != 3:
+    print \
+    '''Need to give -l for model loading or -s for model saving \
+followed by file name'''
+    sys.exit()
 
 # Load and clean train data
 df = pd.read_csv("data/items_otodom.pl_270316_8.csv")
@@ -107,25 +115,30 @@ import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
     level=logging.INFO)
 
-# Set values for various parameters
-num_features = 300    # Word vector dimensionality                      
-min_word_count = 40   # Minimum word count                        
-num_workers = 4       # Number of threads to run in parallel
-context = 10          # Context window size
-downsampling = 1e-3   # Downsample setting for frequent words
-
 # Initialize and train the model (this will take some time)
 from gensim.models import word2vec
-print "Training model..."
-model = word2vec.Word2Vec(sentences, workers=num_workers, \
+if sys.argv[1] == "-s":
+    # Set values for various parameters
+    num_features = 300    # Word vector dimensionality                      
+    min_word_count = 40   # Minimum word count                        
+    num_workers = 4       # Number of threads to run in parallel
+    context = 10          # Context window size
+    downsampling = 1e-3   # Downsample setting for frequent words
+    print "Training model..."
+    model = word2vec.Word2Vec(sentences, workers=num_workers, \
             size=num_features, min_count = min_word_count, \
             window = context, sample = downsampling)
 
-# If you don't plan to train the model any further, calling 
-# init_sims will make the model much more memory-efficient.
-model.init_sims(replace=True)
-
-# It can be helpful to create a meaningful model name and 
-# save the model for later use. You can load it later using Word2Vec.load()
-model_name = "300features_40minwords_10context"
-model.save(model_name)
+    # If you don't plan to train the model any further, calling 
+    # init_sims will make the model much more memory-efficient.
+    model.init_sims(replace=True)
+    
+    # It can be helpful to create a meaningful model name and 
+    # save the model for later use. You can load it later using Word2Vec.load()
+    model.save(sys.argv[2])
+if sys.argv[1] == "-l":
+    # Load model
+    model = word2vec.Word2Vec.load(sys.argv[2])
+# Test model
+model.doesnt_match("zlew wanna prysznic piekarnik".split())
+model.most_similar("wanna")
