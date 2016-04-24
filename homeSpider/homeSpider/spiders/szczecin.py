30890c6a3d71ed3cf9282f7cad6dcfed24d7dd3a
Mateusz Najsztub <mateusz.najsztub@gmail.com>
2016-04-24 23:22:07 +0200

    Remove junk, add comments

--- homeSpider/homeSpider/spiders/szczecin.py
+++ homeSpider/homeSpider/spiders/szczecin.py
@@ -10,10 +10,12 @@ class SzczecinSpider(CrawlSpider):
         'http://otodom.pl/sprzedaz/mieszkanie/szczecin/?search[dist]=0',
     )
     rules = (
+        # Follow pages
         Rule(LinkExtractor(allow=('page=[0-9]+'),
              restrict_xpaths=('//ul[@class="pager"]',)),
              follow=True,
         ),
+        # Extract only adds
         Rule(LinkExtractor(allow=('\.html' ),
                            restrict_xpaths=('//div[@class="col-md-content"]',),
                            deny=('dom-','dzialka','garaz','hala', 'uzytkowy', 'wynajem')), callback='parse_page'),
@@ -26,11 +28,4 @@ class SzczecinSpider(CrawlSpider):
         with open(filename, 'wb') as f:
             f.write(response.body)'''
 
-'''Rule(LinkExtractor(deny=('dom-'))),
-Rule(LinkExtractor(deny=('dzialka'))),
-Rule(LinkExtractor(deny=('garaz'))),
-Rule(LinkExtractor(deny=('hala'))),
-Rule(LinkExtractor(deny=('uzytkowy'))),
-Rule(LinkExtractor(deny=('wynajem'))),
-'''
-#Rule(LinkExtractor(allow='\.html$', ), follow=True, callback='parse_page')
+
