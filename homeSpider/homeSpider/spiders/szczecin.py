2190edc414d34d53d0dbe8dbf5ee3e9ecd4b0d8c
Mateusz Najsztub <mateusz.najsztub@gmail.com>
2016-04-24 22:14:07 +0200

    Added a CrawlSpider to szczecin

--- homeSpider/homeSpider/spiders/szczecin.py
+++ homeSpider/homeSpider/spiders/szczecin.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-
-class SzczecinSpider(scrapy.Spider):
+class SzczecinSpider(CrawlSpider):
     name = "szczecin"
-    allowed_domains = ["http://otodom.pl/sprzedaz/mieszkanie/szczecin/"]
+    allowed_domains = ["otodom.pl"]
     start_urls = (
-        'http://www.http://otodom.pl/sprzedaz/mieszkanie/szczecin/?search[dist]=0/',
+        'http://otodom.pl/sprzedaz/mieszkanie/szczecin/?search[dist]=0',
+    )
+    rules = (
+        Rule(LinkExtractor(allow='\.html$', ), follow=True, callback='parse_page'),
+        Rule(LinkExtractor(allow='sprzedaz/mieszkanie/szczecin/\?search%5Bdescription%5D=1&search%5Bdist%5D=0&page=[0-9]+'), follow=True,
+        ),
+        Rule(LinkExtractor(deny=('dom-'))),
+        Rule(LinkExtractor(deny=('dzialka'))),
+        Rule(LinkExtractor(deny=('garaz'))),
+        Rule(LinkExtractor(deny=('hala'))),
+        Rule(LinkExtractor(deny=('uzytkowy'))),
+        Rule(LinkExtractor(deny=('wynajem'))),
     )
 
-    def parse(self, response):
-        pass
+    def parse_page(self, response):
+        self.logger.info('Hi, this is an item page! %s', response.url)
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
